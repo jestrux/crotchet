51be611ab2b0ac526530a5359d008aa6e0d8b345
Walter Kimaro <wakyj07@gmail.com>
2024-08-13 10:40:41 +0300

    Add watchlist card to reader

--- desktop/extensions/reader.ts
+++ desktop/extensions/reader.ts
@@ -1,5 +1,60 @@
 import "../@types/index";
 
+registerWidget("readerWatchList", {
+	title: "Watch List",
+	resolve: async () => {
+		const res = await sourceGet(
+			{ handler: () => queryDb("reader") },
+			{ orderBy: "_index,desc", filters: { group: "📺 Watch" }, limit: 3 }
+		);
+		return res?.map((item) => {
+			const isVideo =
+				item.url?.toLowerCase().indexOf("videos") != -1 ||
+				item.url?.toLowerCase().indexOf("youtube") != -1 ||
+				item.group?.toLowerCase().indexOf("watch") != -1;
+
+			return {
+				...item,
+				...(isVideo
+					? { video: item.image || "placeholder" }
+					: { image: item.image || "placeholder" }),
+				title: item.title || "Untitled" + (" " + item.group),
+				subtitle: item.description,
+				tags: [item.group],
+			};
+		});
+	},
+	filter: () => {
+		return {
+			field: "group",
+			choices: [
+				{ label: "All", value: "" },
+				"📺 Watch",
+				"🧪 Learn",
+				"🎧 Listen",
+				"🌎 General",
+			],
+			defaultValue: "",
+		};
+	},
+	content: UI.List,
+	// actions: [{ label: "Add Entry", icon: UI.Icon("add"), handler: () => {} }],
+	actionButton({ data }) {
+		if (!data?.length) return;
+		return {
+			icon: UI.Icon("add-circle"),
+			label: "Preview First",
+			handler: () => openUrl(data[0].url),
+		};
+	},
+
+	listenForUpdates: (callback = () => {}) => {
+		const event = "firebase-table-updated:readingList";
+		window.addEventListener(event, callback, false);
+		return () => window.removeEventListener(event, callback, false);
+	},
+});
+
 registerWidget("readingList", {
 	title: "Reader",
 	resolve: async () => {
